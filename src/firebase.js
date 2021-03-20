64d8f2e90575c7ac4d7aadacc3670ac1374edaf2
Codeliner <codeliner96@gmail.com>
2021-03-21 01:27:54 +0900

    Added react router dom

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,13 @@
 import firebase from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDD_gBCB00sVIrmlmhEgPkFQParVMA5NpI",
-  authDomain: "kwitter-6b1fa.firebaseapp.com",
-  projectId: "kwitter-6b1fa",
-  storageBucket: "kwitter-6b1fa.appspot.com",
-  messagingSenderId: "1011097294194",
-  appId: "1:1011097294194:web:0a2d0ad6ecb4e65ae9b9eb",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebase.initializeApp(firebaseConfig);
