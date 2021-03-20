2494c3f6940f2d662642a90d6222f56fde6dd54b
Codeliner <codeliner96@gmail.com>
2021-03-21 00:51:48 +0900

    initialize firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDD_gBCB00sVIrmlmhEgPkFQParVMA5NpI",
+  authDomain: "kwitter-6b1fa.firebaseapp.com",
+  projectId: "kwitter-6b1fa",
+  storageBucket: "kwitter-6b1fa.appspot.com",
+  messagingSenderId: "1011097294194",
+  appId: "1:1011097294194:web:0a2d0ad6ecb4e65ae9b9eb",
+};
+
+export default firebase.initializeApp(firebaseConfig);
